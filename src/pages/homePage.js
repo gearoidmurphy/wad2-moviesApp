23abde6a236018f425f6b800893dc7b1129cdd47
Gearoid Murphy <gearoidmurphy2000@gmail.com>
2020-11-02 16:13:30 +0000

    Add to favorites logic completed; Dummy favorites page.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect } from "react";
 import Header from "../components/headerMovieList";
 import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
-
+import StubAPI from "../api/stubAPI";
 
 const MovieListPage = () => {
     const [titleFilter, setTitleFilter] = useState("");       
@@ -36,13 +36,26 @@ const MovieListPage = () => {
       if (type === "name") setTitleFilter(value);
       else setGenreFilter(value);
     };
-    return (
-    <>
-      <Header numMovies={displayedMovies.length} />          {/* CHANGED */}
-      <FilterControls onUserInput={handleFilterChange} />    {/* CHANGED */}
-      <MovieList movies={displayedMovies} />                  {/* CHANGED */}
-    </>
-  );
-};
-
-export default MovieListPage;
+    const addToFavorites = movieId => {
+        // Find index position of selected movie in the list
+        const index = movies.map(m => m.id).indexOf(movieId)
+    
+        StubAPI.add(movies[index])
+        const updatedList = [...movies]  // Make a copy of the movie list
+        updatedList.splice(index, 1)     // Remove selected movie from home page list
+        setMovies(updatedList)  
+      }
+    
+      return (
+        <>
+          <Header numMovies={displayedMovies.length} />
+          <FilterControls onUserInput={handleFilterChange} />
+          <MovieList
+            movies={displayedMovies}
+            buttonHandler={addToFavorites}
+          />     {/* CHANGED */}
+        </>
+      );
+    };
+    
+    export default MovieListPage;
